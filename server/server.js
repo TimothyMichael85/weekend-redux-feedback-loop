01e92ce792c9a62632b5322cce7121fe5458fca3
Tim Anderson <t.anderson0085@gmail.com>
2022-08-21 13:24:36 -0500

    added surveyRouter to server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+const surveyRouter = require('./modules/survey.router')
+app.use('/feedback', surveyRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
