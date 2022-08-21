50fb43c935783944df7f549759c71edc48d2f777
Tim Anderson <t.anderson0085@gmail.com>
2022-08-21 13:17:35 -0500

    wrote router POST

--- server/modules/survey.router.js
+++ server/modules/survey.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../pool');
 
-//GET existing feedback info from  "prime_feedback" DB
+//GET existing feedback info from "prime_feedback" DB
 router.get('/', (req, res) => {
     const queryText = `SELECT * FROM "feedback:`;
 
@@ -13,9 +13,31 @@ router.get('/', (req, res) => {
         }).catch(err => {
             console.log('error on router GET', err);
             res.sendStatus(500);
-        })
-})
+        });
+});
 //end GET
 
+//POST to DB
+router.post('/', (req,res) => {
+    const newSurvey = req.body;
+    console.log('New Survey Results ',newSurvey);
+    
+    const queryText =`
+        INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+        VALUES ($1, $2, $3, $4);
+        `;
+    pool.query(queryText,[
+        Number(newSurvey.feeling),
+        Number(newSurvey.understanding),
+        Number(newSurvey.support),
+        newSurvey.comments])
+        .then((result) => {
+            res.sendStatus(201);
+        }).catch((error) => {
+            console.log('Error in POST to DB', error)
+            res.sendStatus(500);
+        });
+});
+//end POST
 
 module.exports = router;
