cb12cf90cbbb76c562c036dc20b87181a466614a
Tim Anderson <t.anderson0085@gmail.com>
2022-08-21 17:02:05 -0500

    completed page loops back to start. I did it!!!!

--- src/components/Completed/Completed.jsx
+++ src/components/Completed/Completed.jsx
@@ -1,7 +1,27 @@
+import { useHistory } from 'react-router-dom';
+
+
 function Completed(){
+    const history = useHistory();
+
+    const over = () => {
+        console.log('its over');
+        history.push('/');
+    }
 
     return (
-        <h2>Completed Page</h2>
+        <>
+        <h2>YOU DID IT!!!!!!</h2>
+        <h3>We knew you could!</h3>
+
+        <h5>Click the button to go back and start a new one
+            <br></br>
+            Or go home. You look sleepy 
+        </h5>
+        <button onClick={over}>PEACE OUT!!!</button>
+
+        </>
+        
     )
 }
 
