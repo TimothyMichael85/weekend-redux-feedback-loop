926d23632a178d43cae391f72f53e261f4421bb6
Tim Anderson <t.anderson0085@gmail.com>
2022-08-21 13:01:18 -0500

    wrote router GET

--- server/modules/survey.router.js
+++ server/modules/survey.router.js
@@ -2,5 +2,20 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../pool');
 
+//GET existing feedback info from  "prime_feedback" DB
+router.get('/', (req, res) => {
+    const queryText = `SELECT * FROM "feedback:`;
+
+    pool
+        .query(queryText)
+        .then (result => {
+            res.send(result.row);
+        }).catch(err => {
+            console.log('error on router GET', err);
+            res.sendStatus(500);
+        })
+})
+//end GET
+
 
 module.exports = router;
